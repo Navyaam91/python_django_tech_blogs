95268b4e26ecb6f4cb032f23abda1797540ad1e5
Navya <amnavya91@gmail.com>
2026-02-11 19:35:14 +0530

    admin change

--- create_admin.py
+++ create_admin.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techblogs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 django.setup()
 
 User = get_user_model()
